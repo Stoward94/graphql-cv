f23210de83880fd3df52bc2a8fb86585890b62e9
Luke Stoward <luk_stoward@hotmail.co.uk>
2017-08-14 21:30:51 +0100

    simple Express middleware added§

--- server.js
+++ server.js
@@ -9,11 +9,27 @@ const jsondb = require('./lib/jsondb');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.redirect('/graphql');
+});
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true,
   context: {jsondb}
 }));
+
+// 404 Middleware
+app.use((req, res, next) => {
+  const msg = '404 - Have you tried the <a href="/">GraphQL</a> interface?';
+  res.status(404).send(msg);
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('500 - Looks like we have a problem...');
+});
 
 app.listen(config.server.port);
 
